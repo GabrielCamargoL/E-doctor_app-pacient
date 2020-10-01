a8151ee9cbc4f9698c4998a62c6ad1a1eee292b5
thhenrique <henriquethiago298@gmail.com>
2020-10-01 01:03:03 -0300

    add useMemo, useCallback in shedule

--- src/pages/Schedule/index.js
+++ src/pages/Schedule/index.js
@@ -1,8 +1,8 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, {useState, useEffect} from 'react';
+import React, {useState, useMemo, useCallback} from 'react';
 import {ScrollView, View, TouchableOpacity} from 'react-native';
 
-import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import { Agenda, LocaleConfig } from 'react-native-calendars';
 import {
   Container,  
   HeaderText,  
@@ -23,12 +23,14 @@ const timeToString = (time) => {
 
 export default function Shedule({navigation}) {
   const [value, setValue] = useState([]);
+  const [daySelected, setDaySelected] = useState('')
   const vacation = { key: 'vacation', color: 'red', selectedDotColor: 'blue' };
   const massage = { key: 'massage', color: 'blue', selectedDotColor: 'blue' };
   const workout = { key: 'workout', color: 'green' };
   const [heigth, setHeigth] = useState(0)
   const [items, setItems] = useState({});
   
+  const memoizedValue = useMemo(() => renderItem, [items])
 
   function find_dimesions(layout){
     const {x, y, width, height} = layout;
@@ -37,61 +39,99 @@ export default function Shedule({navigation}) {
     console.log(width);
     console.log(height);
     setHeigth(height)
-  }
+  }  
 
-  const loadItems = (day) => {
-    setTimeout(() => {
-      for (let i = -15; i < 85; i++) {
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        const strTime = timeToString(time);
-        if (!items[strTime]) {
-          items[strTime] = [];
-          const numItems = Math.floor(Math.random() * 3 + 1);
-          for (let j = 0; j < numItems; j++) {
-            items[strTime].push({
-              name: 'Item for ' + strTime + ' #' + j,
-              height: Math.max(50, Math.floor(Math.random() * 150))
-            });
-          }
+  LocaleConfig.locales['br'] = {
+    monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+    monthNamesShort: ['Jan.','Fev.','Maio','Abril','Maio','Jun','Jul.','Ago','Set.','Out.','Nov.','Dez.'],
+    dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'],
+    dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
+    today: 'Hoje'
+  };
+  LocaleConfig.defaultLocale = 'br';
+
+  const loadItems = (day) => {    
+    for (let i = -15; i < 85; i++) {
+      const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+      const strTime = timeToString(time);
+      if (!items[strTime]) {
+        items[strTime] = [];
+        const numItems = Math.floor(Math.random() * 3 + 1);
+        for (let j = 0; j < numItems; j++) {
+          items[strTime].push({
+            name: 'Item for ' + strTime + ' #' + j,
+            height: Math.max(50, Math.floor(Math.random() * 150))
+          });
         }
       }
-      const newItems = {};
-      Object.keys(items).forEach(key => {newItems[key] = items[key];});
-      setItems(newItems);
-    }, 1000);
+    }
+    const newItems = {};
+    Object.keys(items).forEach(key => {newItems[key] = items[key];});
+    setItems(newItems);    
   }
-
-  const renderItem = (item) => {
+ 
+  const renderItem = (props)  => {
+    console.log(props);
     return (
       <TouchableOpacity style={{marginTop: 17, marginRight: 10}}>
-        <Container>         
-
+        <Container>      
           <View style={{flexDirection: 'row', justifyContent: "space-between", alignItems: 'center'}}>
             <HeaderText>
-              {item.name}
+              {props.name}
             </HeaderText>
             <Logo source={logo} resizeMode="contain" />
-          </View>
-         
+          </View>     
         </Container>
-      </TouchableOpacity>
-    )
-  }
+      </TouchableOpacity>    
+  )}
 
   return (
     <>
       <Container onLayout={(event) => {find_dimesions(event.nativeEvent.layout) }}>
-        <View>
-          {/* <HeaderText>Selecione a data para consulta</HeaderText> */}
+        <ScrollView>
+          <HeaderText>Selecione a data para consulta</HeaderText>
           <Agenda            
             items={items}
             loadItemsForMonth={loadItems}
-            selected={'2017-05-16'} 
-            renderItem={renderItem}           
+            selected={'2020-01-01'} 
+            renderItem={memoizedValue}
+            minDate={'2019-05-10'}   
+            maxDate={'2025-05-30'}
+            onDayPress={(day) => setDaySelected(day.dateString)}     
+            pastScrollRange={10}
+            futureScrollRange={10}
+            style={{height: 300}}
+            theme={{
+              backgroundColor: '#ffffff',
+              calendarBackground: '#ffffff',
+              textSectionTitleColor: '#b6c1cd',
+              textSectionTitleDisabledColor: '#d9e1e8',
+              selectedDayBackgroundColor: '#00adf5',
+              selectedDayTextColor: '#ffffff',
+              todayTextColor: '#00adf5',
+              dayTextColor: '#2d4150',
+              textDisabledColor: '#d9e1e8',
+              dotColor: '#00adf5',
+              selectedDotColor: '#ffffff',
+              arrowColor: 'orange',
+              disabledArrowColor: '#d9e1e8',
+              monthTextColor: 'blue',
+              indicatorColor: 'blue',
+              textDayFontFamily: 'monospace',
+              textMonthFontFamily: 'monospace',
+              textDayHeaderFontFamily: 'monospace',
+              textDayFontWeight: '300',
+              textMonthFontWeight: 'bold',
+              textDayHeaderFontWeight: '300',
+              textDayFontSize: 16,
+              textMonthFontSize: 16,
+              textDayHeaderFontSize: 16
+            }}  
           />
-          {/* <Row>
+
+          <Row>
             <HeaderText>DIA SELECIONADO:</HeaderText>
-            <HeaderText>08/11/2001</HeaderText>
+            <HeaderText>{daySelected}</HeaderText>
           </Row>
           <Row>
             <HeaderText>HORÁRIO:</HeaderText>
@@ -99,8 +139,8 @@ export default function Shedule({navigation}) {
           </Row>          
             <Button onPress={() => {}}>
               <ButtonText>solicitar agendamento</ButtonText>
-            </Button>           */}
-        </View>
+            </Button>          
+        </ScrollView>
       </Container>
     </>
   );
